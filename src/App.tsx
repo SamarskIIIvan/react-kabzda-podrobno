e3bfcdd188722b309a2e107fc740f31350844707
Samarskiy.ivan <sa20201996@mail.ru>
2023-07-09 16:17:35 +0200

    add first component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+       <Rating/>
     </div>
   );
 }
+
+
+function Rating(){
+  return(
+    <>
+    <div>star1</div>
+    <div>star2</div>
+    <div>star3</div>
+    <div>star4</div>
+    <div>star5</div>
+    </>
+  )
+}
+
 
 export default App;
