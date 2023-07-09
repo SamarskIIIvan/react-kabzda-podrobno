2d8b53cf6f19331f8960dfb07b41689f47b51176
Samarskiy.ivan <sa20201996@mail.ru>
2023-07-09 17:20:51 +0200

    add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import './App.css';
+        import React from 'react';
+        import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-       <Rating/>
-    </div>
-  );
-}
+        type TitlePropsType = {
+          title: string
+        }
+
+        function App() {
+          return (
+            <div className="App">
+              <Title title="This`s Component"/>
+              <Rating/>
+              <Accordion/>
+            </div>
+          );
+        }
+
+        function Title(props:TitlePropsType){
+          return(
+           <h3>
+           {props.title}
+           </h3>
+          )
+        }
+
+        function Rating(){
+          return(
+            <>
+           <Star/>
+           <Star/>
+           <Star/>
+           <Star/>
+           <Star/>
+            </>
+          )
+        }
+
+        function Star(){
+          return(
+            <div>
+            Star
+            </div>
+          )
+        }
 
 
-function Rating(){
-  return(
-    <>
-    <div>star1</div>
-    <div>star2</div>
-    <div>star3</div>
-    <div>star4</div>
-    <div>star5</div>
-    </>
-  )
-}
+        function Accordion(){
+          return(
+            <>
+           <Title title='Menu' />
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+              <li>5</li>
+            </ul>
+            </>
+          )
+        }
 
 
-export default App;
+        export default App;
